367c5169b05c638d7208ef4c8e66160166a7c893
Abdumalik001 <abdumaliksapoqulov@gmail.com>
2021-09-16 10:49:15 +0500

    add converter

---
+++ lib/converter_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ConverterScreen extends StatefulWidget {
+  const ConverterScreen({Key key}) : super(key: key);
+
+  @override
+  _ConverterScreenState createState() => _ConverterScreenState();
+}
+
+class _ConverterScreenState extends State<ConverterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
